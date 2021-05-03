55517cd8a85a4fce867f66a7abe9faf35c6c96be
teefats <tee_fats@yahoo.com>
2021-05-03 17:23:20 +0100

    Added models

--- permits/permit_tracker/admin.py
+++ permits/permit_tracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Permit
+# # Register your models here.
+# class PermitAdmin(admin.ModelAdmin):
+#     pass
+admin.site.register(Permit)
--- permits/permit_tracker/models.py
+++ permits/permit_tracker/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Permit(models.Model):
+    name = models.CharField(max_length=50, default=None)
+    permit_type = models.CharField(max_length=100)
+    permit_conditions = models.TextField()
+    permit_authority = models.CharField(max_length=200)
+    list_sites = (
+        ('Gay', 'Gaydon'),
+        ('W1', 'Wellesbourne Unit 1'),
+        ('W2', 'Wellesbourne Unit 2'),
+        ('SA', 'St. Athan'),
+        ('WO', 'Wolverton Mill'),
+        ('NP', 'Newport Pagnell'),
+
+
+    )
+    site = models.CharField(max_length=3, choices=list_sites)
+    area = models.CharField(max_length=200)
+    comments = models.TextField()
+    issue_date = models.DateField()
+    expiration_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+    
+
+
