089049e733e29b5324f6cfc6aa4217c1cff9b460
teefats <tee_fats@yahoo.com>
2021-05-03 19:10:27 +0100

    Edited admin

--- permits/permit_tracker/admin.py
+++ permits/permit_tracker/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Permit
 # # Register your models here.
-# class PermitAdmin(admin.ModelAdmin):
-#     pass
-admin.site.register(Permit)
+@admin.register(Permit)
+class PermitAdmin(admin.ModelAdmin):
+    list_display = ['name', 'permit_type', 'permit_conditions', 'permit_authority', 'issue_date','expiration_date']
+    date_hierarchy ='issue_date'
+    
